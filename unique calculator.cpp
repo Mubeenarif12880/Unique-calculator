7d89983da3aa4438c8e940875ec79fe2a1e7b85c
Mubeenarif12880 <116204736+Mubeenarif12880@users.noreply.github.com>
2022-10-21 20:02:59 +0500

    Add files via upload

---
+++ unique calculator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1 , num2;
+	char x;
+	while(true)
+	{
+		cout<<"Enter your first number"<<endl;
+		cin>>num1;
+		cout<<"Enter your second number"<<endl;
+		cin>>num2;
+		
+		cout<<"press + for addition"<<endl;
+		cout<<"press - for subtraction"<<endl;
+		cout<<"press * for multiplication"<<endl;
+		cout<<"press / for division"<<endl;
+		cout<<"press % for reminder"<<endl;
+		
+		cin>>x;
+		if(x=='+')
+		cout<<"the sum ="<<num1+num2<<endl;
+		else if(x=='-')
+		cout<<"the subtraction ="<<num1-num2<<endl;
+		else if(x=='*')
+		cout<<"the multiplication ="<<num1*num2<<endl;
+		else if(x=='/')
+		cout<<"the division ="<<num1/num2<<endl;
+		else if(x=='%')
+		cout<<"the reminder ="<<num1%num2<<endl;
+		
+		else
+		cout<<"Invalid operation"<<endl;
+	}
+	
+	return 0;
+	
+}
